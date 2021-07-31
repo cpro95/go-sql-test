83c83d1a523194bba73219743f0ff48c1966cdc4
unknown <cpro95@gmail.com>
2021-08-01 08:20:00 +0900

    ui input now has no q key quit and db updated

--- main.go
+++ main.go
@@ -109,8 +109,8 @@ func main() {
 		AddItem(ui.input, 2, 1, true).
 		AddItem(ui.resultLabel, 2, 2, false).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
-			AddItem(ui.list, 0, 2, false).
-			AddItem(ui.infoView, 0, 3, false).
+			AddItem(ui.list, 0, 4, false).
+			AddItem(ui.infoView, 0, 5, false).
 			AddItem(tview.NewTextView(), 0, 1, false), 0, 1, false)
 		// last TextView is a right margin for infoView
 
@@ -150,19 +150,21 @@ func main() {
 
 			// goint to first items
 			if ch == 'g' {
-				if ui.firstG == false {
+				if !ui.firstG {
 					ui.firstG = true
 				} else {
 					ui.list.SetCurrentItem(0)
 					ui.firstG = false
 				}
 
-			} else if ui.firstG == true {
+			} else if ui.firstG {
 				ui.firstG = false
 			}
 
 			if ch == 'Q' || ch == 'q' {
-				handleQuit(ui)
+				if ui.list.HasFocus() {
+					handleQuit(ui)
+				}
 			}
 
 		}
